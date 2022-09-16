ca35582781c38de9d50a51ce824a3678edc6ee1b
Vasyl Lespuch <v.lespukh@gmail.com>
2022-09-16 21:49:43 +0200

    prime numbers initial commit

---
+++ Prime numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number you want to check: ");
+            int number = Int32.Parse(Console.ReadLine());
+            int divisor = 0;
+
+            for ( var i = 1; i <= number; i++)
+            {
+                if (number %i== 0)
+                {
+                    divisor++;
+                }
+            }
+            if (divisor == 2)
+            {
+                Console.WriteLine("Entered Numbe is a Prime Number");
+            }else
+            {
+                Console.WriteLine("Entered Number is not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
